c35783db7fac868bb0ab322ecacacf5bc37bf8f9
Tommaso Zoppi <tommaso.zoppi@unifi.it>
2016-05-16 14:49:47 +0200

    asas

---
+++ AD_MultiLayer_Commons/src/ippoz/multilayer/commons/datacategory/DataCategory.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package ippoz.multilayer.commons.datacategory;
+
+/**
+ * The Enum DataCategory.
+ *
+ * @author Tommy
+ */
+public enum DataCategory {
+	
+	/** The plain data category: the normal observation. */
+	PLAIN,
+	
+	/** The diff data category: difference between current and previous observation. */
+	DIFF
+}
